430a55617bef98021a986ba72a8903cc8a687b13
Muhammad Mohsin <m.mohsin.zafar@outlook.com>
2022-12-26 02:01:23 +0500

    M: Registration Form
    Added fields as per the API

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {Router} from "@angular/router";
 
@@ -18,25 +18,45 @@ export class SignupComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  prepareForm(){
+  prepareForm() {
     return new FormGroup({
-      'name': new FormControl('', Validators.required),
+      'firstName': new FormControl('', Validators.required),
+      'middleName': new FormControl(''),
+      'lastName': new FormControl('', Validators.required),
+      'cnic': new FormControl('', Validators.required),
+      'gender': new FormControl('', Validators.required),
       'email': new FormControl('', Validators.email),
       'password': new FormControl('', Validators.required),
-      'confirmPassword': new FormControl('', Validators.required)
+      'confirmPassword': new FormControl('', Validators.required),
+      'imageFile': new FormControl('', Validators.required),
+      'imageSrc': new FormControl('', Validators.required),
     })
   }
 
   signup(): void {
     let user = this.signupForm.value;
-    // Perform some validations maybe
+    // Perform some validations
     delete user['confirmPassword'];
-
+    delete user['imageFile']
+    console.log(user);
     // this._userService.registerUser(user).subscribe((response) => {
     //   if(response?.id){
     //     this._router.navigate(['/']);
     //   }
     //
     // });
+  }
+
+  onImageSelected(e: any) {
+    if (e.target.files.length > 0) {
+      const file = e.target.files[0];
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        this.signupForm.patchValue({
+          imageSrc: reader.result
+        });
+      };
+    }
   }
 }
--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,2 +1,8 @@
 export interface User {
+  fullName: string;
+  email: string;
+  cnic: string;
+  gender: string;
+  imageSrc: string;
+  password: string;
 }
